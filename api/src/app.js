f48f9eaaaeb13e69c77aa747653592deae2c2e05
Maximiliano <maxi950live@gmail.com>
2022-12-26 19:47:08 -0300

    asd

--- api/src/app.js
+++ api/src/app.js
@@ -16,14 +16,14 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://countries-five-coral.vercel.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
 server.use(cors({
-  origin: ["http://localhost:3000", 'https://countries-five-coral.vercel.app'],
+  origin: ['https://countries-five-coral.vercel.app'],
   methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }));
 
